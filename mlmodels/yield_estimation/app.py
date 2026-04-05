499c334cda999f060c03cd81b8a78b4b00dee48a
Harshit-Bhan <harshitbhan2004@gmail.com>
2026-04-05 11:15:58 +0530

    Imp fixes for deployment

--- mlmodels/yield_estimation/app.py
+++ mlmodels/yield_estimation/app.py
@@ -79,4 +79,5 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=False)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port, debug=False)
